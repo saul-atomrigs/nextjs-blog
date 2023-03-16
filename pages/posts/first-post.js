866c3f4357022da1801487e0a6af44c16899a1b5
Saul Lee <solleedata@gmail.com>
2023-03-16 20:57:41 +0900

    Image component (next/Image)

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function FirstPost() {
   return (
@@ -7,6 +8,13 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={144} // Desired size with correct aspect ratio
+        width={144} // Desired size with correct aspect ratio
+        alt="Your Name"
+      />
     </>
   );
 }
