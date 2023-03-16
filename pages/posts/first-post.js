1396c3ba2a4a073bde05c66f3ae92e5bcd6d7b75
Saul Lee <solleedata@gmail.com>
2023-03-17 08:12:25 +0900

    Script component (next/script)

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,22 @@
 import Link from "next/link";
 import Image from "next/image";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
     <>
       <Head>
         <title>First Post</title>
+        {/* <script src="https://connect.facebook.net/en_US/sdk.js" /> */}
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
